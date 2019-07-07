1d772f0035a5b869e164abf0a3486b408d800a8b
healthypackrat <healthypackrat@gmail.com>
2019-07-08 00:14:55 +0900

    Only admins can see the links to create and delete projects

---
+++ spec/system/hidden_links_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.feature "Users can only see the appropriate links", type: :system do
+  given(:project) { FactoryBot.create(:project) }
+  given(:user) { FactoryBot.create(:user) }
+  given(:admin) { FactoryBot.create(:user, :admin) }
+
+  context "anonymous users" do
+    scenario "cannot see the New Project link" do
+      visit "/"
+      expect(page).not_to have_link "New Project"
+    end
+
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      expect(page).not_to have_link "Delete Project"
+    end
+  end
+
+  context "regular users" do
+    background { login_as(user) }
+
+    scenario "cannot see the New Project link" do
+      visit "/"
+      expect(page).not_to have_link "New Project"
+    end
+
+    scenario "cannot see the Delete Project link" do
+      visit project_path(project)
+      expect(page).not_to have_link "Delete Project"
+    end
+  end
+
+  context "admin users" do
+    background { login_as(admin) }
+
+    scenario "can see the New Project link" do
+      visit "/"
+      expect(page).to have_link "New Project"
+    end
+
+    scenario "can see the Delete Project link" do
+      visit project_path(project)
+      expect(page).to have_link "Delete Project"
+    end
+  end
+end
