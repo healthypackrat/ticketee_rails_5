ad0b6b3a406e194b68e7cd7c107cfe077dee2f1c
healthypackrat <healthypackrat@gmail.com>
2019-07-11 19:46:42 +0900

    Handle authorization errors gracefully

--- spec/requests/projects_spec.rb
+++ spec/requests/projects_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe "Projects", type: :request do
     message = "The project you were looking for could not be found."
     expect(flash[:alert]).to eq message
   end
+
+  it "handles permission errors by redirecting to a safe place" do
+    project = FactoryBot.create(:project)
+    get project_path(project)
+
+    expect(response).to redirect_to(root_path)
+    message = "You aren't allowed to do that."
+    expect(flash[:alert]).to eq message
+  end
 end
